0e116c9faff90afe4db3e50398d71b52d390f26a
Bia Delmont <biadelmont@gmail.com>
2020-05-08 22:16:29 -0300

    Criando retorno da palavra sorteada

--- src/main/java/br/com/bootCamp/jogo/Forca.java
+++ src/main/java/br/com/bootCamp/jogo/Forca.java
@@ -1,9 +1,47 @@
 package br.com.bootCamp.jogo;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Forca {
 	
 	public static void main(String[] args) {
+		System.out.println("Bem vindo ao jogo!");
+		Scanner in = new Scanner(System.in);
+		System.out.print("Qual a dificuldade do jogo?: F(Fácil); N(Normal); D(Dificil) -->");
+		String dificuldadeJogo = in.next();
 		
+		switch(dificuldadeJogo) {
+			case "F":
+				System.out.println(listaPalavrasFacil());
+				break;
+			case "N":
+				System.out.println(listaPalavrasNormal());
+				break;
+			case "D":
+				System.out.println(listaPalavrasDificil());
+				break;
+			default:
+				System.out.println("Opção não encontrada");
+		}
+	}
+	
+	public static String listaPalavrasDificil() {
+		String[] palavras = {"carambola", "lichia", "jatoba"};
+		Random random = new Random();
+		return palavras[random.nextInt(palavras.length-1)];
+	}
+	
+	public static String listaPalavrasNormal() {
+		String[] palavras = {"abacaxi", "melancia", "melao"};
+		Random random = new Random();
+		return palavras[random.nextInt(palavras.length-1)];
+	}
+	
+	public static String listaPalavrasFacil() {
+		String[] palavras = {"banana", "uva", "maca"};
+		Random random = new Random();
+		return palavras[random.nextInt(palavras.length-1)];
 	}
 
 }
